3a6ba74268c8fa990fc80c3d210099fabb5d9b83
aseering <aseering@46cb7e34-da67-11de-849f-b362a63f070a>
2009-12-11 04:17:28 +0000

    Fix up __BIG_ENDIAN/__LITTLE_ENDIAN defines.  Also includes some tweaks to debugging printed info, etc

--- trunk/native_client/src/third_party/nacl_sdk/linux/sdk/nacl-sdk/nacl/include/machine/endian.h
+++ trunk/native_client/src/third_party/nacl_sdk/linux/sdk/nacl-sdk/nacl/include/machine/endian.h
@@ -4,16 +4,20 @@
 
 #ifndef BIG_ENDIAN
 #define BIG_ENDIAN 4321
+#define __BIG_ENDIAN 4321
 #endif
 #ifndef LITTLE_ENDIAN
 #define LITTLE_ENDIAN 1234
+#define __LITTLE_ENDIAN 1234
 #endif
 
 #ifndef BYTE_ORDER
 #ifdef __IEEE_LITTLE_ENDIAN
 #define BYTE_ORDER LITTLE_ENDIAN
+#define __BYTE_ORDER __LITTLE_ENDIAN
 #else
 #define BYTE_ORDER BIG_ENDIAN
+#define __BYTE_ORDER __BIG_ENDIAN
 #endif
 #endif
 
