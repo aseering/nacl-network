f15ffaf3a89f908c3b33bb72f572bb6c053d4e2d
aseering <aseering@46cb7e34-da67-11de-849f-b362a63f070a>
2009-12-11 18:54:23 +0000

    Remove a bunch of printf's; clean up the code a bit

--- trunk/native_client/src/trusted/service_runtime/linux/nacl_socks_client.c
+++ trunk/native_client/src/trusted/service_runtime/linux/nacl_socks_client.c
@@ -38,74 +38,51 @@ int NaClIsConnectionOk(const struct sockaddr *addr, unsigned char* hash) {
   struct sockaddr_in to;
   int sockfd;
 
-  printf("Allocated memory.  Are we AF_INET?\n");
   if (addr->sa_family != AF_INET) {
     return -1; // We don't support anything != IPv4 at this time
   }
 
-  printf("Checking buffer\n");
   // See if we already know what to do with this IP
   for (i = 0; i < NUM_REMOTE_SERVER_PORTS; i++) {
-    printf("Buffer #%d\n", i);
     if (remoteServerPortsRingbuffer[i].ip == addr_in->sin_addr.s_addr) {
       return verifyServerPort(&remoteServerPortsRingbuffer[i], addr_in->sin_port);
     }
   }
 
   // We don't.  So, go fetch a response, and use it.
-  // Somehow, we have to get our own hash.
-  // For now, just hardcode a magic value.
-  printf("Go make a hash-thing\n");
   MakeNaClHashReq(&buf[0], hash, 0);
   
-
-  printf("Setting up &from\n");
+  // Set up the remote server to send to
   to.sin_port = htons(NACL_VALIDATE_SERVERPORT);
   to.sin_family = AF_INET;
   to.sin_addr = addr_in->sin_addr;
-  /*printf("Set up some in-memory data structures\n");
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_DGRAM;
   
-  printf("Getaddrinfo\n");
-  sprintf(&itoa_buf[0], "%d", NACL_VALDATE_SERVERPORT);
-  if ((r = getaddrinfo(NULL, &itoa_buf[0], &hints, &servinfo)) != 0) {
-    return -1;
-    }*/
-  
-  
-  printf("socket\n");
   if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)  {
-    printf("socket value: %d\n", sockfd);
     return sockfd;
   }
 
   if ((r = connect(sockfd, &to, sizeof(to))) < 0) {
-    printf("Connect failed: %d (%s)\n", r, strerror(errno));
     return r;
   }
   
-  printf("send\n");
   if ((r = send(sockfd, &buf, sizeof(buf), 0)) < 0) {
     return -1;
   }
 
-  printf("recv\n");
+  // Zero ret_buf, so that on the off chance the server
+  // is broken and sends us too little data,
+  // we're conservative and assume not to use unspecified ports.
   memset(&ret_buf[0], 0, sizeof(ret_buf));
   if ((r = recv(sockfd, &ret_buf[0], sizeof(ret_buf), 0)) < 0) {
     return r;
   }
   
-  printf("close\n");
   close(sockfd);
 
-  printf("ParseNaClHashResp\n");
   if ((r = ParseNaClHashResp(&ret_buf[0], sizeof(ret_buf), addr_in->sin_addr.s_addr, &remote_ports, 0)) != 0) {
     return r;
   }
 
-  printf("verifyServerPort and return\n");
   return verifyServerPort(remote_ports, addr_in->sin_port);
 }
 
@@ -131,11 +108,7 @@ int ParseNaClHashResp(const char* buf, uint32_t buf_len, uint32_t server_ip, str
   uint32_t new_nonce;
   struct NaClRemoteServerPorts *p;
 
-  printf("Allocated and UNREF\n");
-
-  printf("Is buf_len is too small?\n");
   if (buf_len < 8200) {
-    printf("Yep: %d < %d\n", buf_len, 8200);
     return -1; // We have to be able to parse out at least our three integers of interest
   }
 
@@ -144,25 +117,20 @@ int ParseNaClHashResp(const char* buf, uint32_t buf_len, uint32_t server_ip, str
   new_nonce = ntohl(int_ptr[1]);
 
   if (success != 0) {
-    printf("We fail: %d\n", success);
     return success;
   }
 
   if (new_nonce != nonce) {
-    printf("Invalid nonce!  Failing...  (0x%x, 0x%x)\n", nonce, new_nonce);
     return -1;
   }
 
-  printf("remoteServerPortsRingbuffer\n");
   p = &remoteServerPortsRingbuffer[rsprb_index++ % NUM_REMOTE_SERVER_PORTS];
 
-  printf("Some memory tweakage\n");
   p->ip = server_ip;
   memcpy(&p->ports[0], buf + 8, 8192);
 
   *ports = p;
 
-  printf("Done\n");
   return 0;
 }
 
